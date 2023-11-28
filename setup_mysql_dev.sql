ce18084f24ee2c6c9d0be869b633d112215aee37
JCRoooD <juancarlos-99@live.com>
2023-11-28 14:03:00 -0400

    added comments

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,5 @@
+-- mysql server for hbnb project
+-- create database and user for hbnb_dev
 CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
