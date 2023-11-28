d8e06cc5d71e06f0a4879064c18c2bb1b40a507c
carlfrank <carl-frank7@hotmail.com>
2023-11-28 14:49:30 -0500

    Changing Init again

--- models/__init__.py
+++ models/__init__.py
@@ -1,22 +1,7 @@
 #!/usr/bin/python3
-"""
-This module initializes the appropriate storage engine
-based on the environment variable HBNB_TYPE_STORAGE.
-"""
+"""This module instantiates an object of class FileStorage"""
+from models.engine.file_storage import FileStorage
 
-import os
 
-# Determine the storage type from the environment variable
-storage_type = os.getenv('HBNB_TYPE_STORAGE')
-
-if storage_type == 'db':
-    # If storage type is 'db', use the DBStorage engine
-    from models.engine.db_storage import DBStorage
-    storage = DBStorage()
-else:
-    # Default to FileStorage if the storage type is not 'db'
-    from models.engine.file_storage import FileStorage
-    storage = FileStorage()
-
-# Reload the storage to either populate from the database or from the JSON file
+storage = FileStorage()
 storage.reload()
