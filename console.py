a8ffbe9e29b1a02b63fe0afffc0106c2ebff29c6
JCRoooD <juancarlos-99@live.com>
2023-11-29 16:05:41 -0400

    fixed console errors

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is '}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -114,34 +114,39 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
-        """ Method to create a new instance of a class """
+        """ Create an object of any class"""
         arg = args.split()
-        if len(args) < 1:
-            print(" class name missing ")
+        if not arg:
+            print("** class name missing **")
             return
-
-        class_name = args[0]
-        if class_name not in self.classes:
-            print(" class doesn't exist ")
+        elif arg[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
             return
-
-        new_instance = self.classes[class_name]()
-        for param in args[1:]:
-            key, value = param.partition('=')
-            if value:
-                if value[0] == '"' and value[-1] == '"':
-                    value = value[1:-1].replace('', ' ')
+        new_instance = HBNBCommand.classes[arg[0]]()
+        for param in arg[1:]:
+            parts = param.split('=')
+            if len(parts) != 2:
+                continue
+            key, value = parts
+            if not hasattr(new_instance, key):
+                continue
+            value = value.replace('_', ' ')
+            if value[0] == '"' and value[-1] == '"':
+                value = value[1:-1].replace('\\"', '"')
+            elif '.' in value:
+                try:
+                    value = float(value)
+                except ValueError:
+                    continue
+            else:
                 try:
                     value = int(value)
                 except ValueError:
-                    try:
-                        value = float(value)
-                    except ValueError:
-                        continue  # Skip if value is not int or float
-                setattr(new_instance, key, value)
-
+                    continue
+            setattr(new_instance, key, value)
         new_instance.save()
         print(new_instance.id)
+
 
     def help_create(self):
         """ Help information for the create method """
@@ -289,7 +294,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -297,10 +302,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
