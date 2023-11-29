9dcfc74f240fb9589153cafeaffa1398af0e5763
carlfrank <carl-frank7@hotmail.com>
2023-11-29 12:09:11 -0500

    testing base model, city, state, init

--- models/base_model.py
+++ models/base_model.py
@@ -1,56 +1,70 @@
 #!/usr/bin/python3
-"""Base_Models Module"""
-
-import uuid
-from datetime import datetime
+"""Defines the BaseModel class."""
 import models
+from uuid import uuid4
+from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column
+from sqlalchemy import DateTime
+from sqlalchemy import String
+
+Base = declarative_base()
 
 
 class BaseModel:
-    """BaseModels class"""
+    """Defines the BaseModel class.
+
+    Attributes:
+        id (sqlalchemy String): The BaseModel id.
+        created_at (sqlalchemy DateTime): The datetime at creation.
+        updated_at (sqlalchemy DateTime): The datetime of last update.
+    """
+
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
-        """Initialize BaseModels instance"""
+        """Initialize a new BaseModel.
+
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+        self.id = str(uuid4())
+        self.created_at = self.updated_at = datetime.utcnow()
         if kwargs:
-            # For each key in the dictionary, set the attribute with \
-            # the associated value.
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    # Convert string datetime to datetime object.
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
-                # Check if 'id' is not in kwargs, then assign it.
-            if "id" not in kwargs.keys():
-                self.id = str(uuid.uuid4())
-            # Check if 'created_at' is not in kwargs, then assign it.
-            if "created_at" not in kwargs.keys():
-                self.created_at = datetime.now()
-            # Check if 'updated_at' is not in kwargs, then assign it
-            if "updated_at" not in kwargs.keys():
-                self.updated_at = datetime.now()
-            if not kwargs:
-                models.storage.new(self)
-        else:
-            """initialize variables"""
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-
-    def __str__(self):
-        """Returns class, Id and dictionary"""
-        return f"[{self.__class__.__name__}] ({self.id}) ({self.__dict__})"
 
     def save(self):
-        """Saves current time to updated_at"""
-        self.updated_at = datetime.now()
+        """Update updated_at with the current datetime."""
+        self.updated_at = datetime.utcnow()
         models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
-        """Create a dictionary and save current data in it"""
-        class_dict = dict(self.__dict__)
-        class_dict["__class__"] = self.__class__.__name__
-        class_dict["created_at"] = self.created_at.isoformat()
-        class_dict["updated_at"] = self.updated_at.isoformat()
-        return class_dict
+        """Return a dictionary representation of the BaseModel instance.
+
+        Includes the key/value pair __class__ representing
+        the class name of the object.
+        """
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = str(type(self).__name__)
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        my_dict.pop("_sa_instance_state", None)
+        return my_dict
+
+    def delete(self):
+        """Delete the current instance from storage."""
+        models.storage.delete(self)
+
+    def __str__(self):
+        """Return the print/str representation of the BaseModel instance."""
+        d = self.__dict__.copy()
+        d.pop("_sa_instance_state", None)
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, d)
--- models/city.py
+++ models/city.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
-"""User module"""
-
+"""Defines the City class."""
+from models.base_model import Base
 from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
-    """City class inheriting BaseModel"""
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """Represents a city for a MySQL database.
 
-    def __str__(self):
-        """Returns a string representation of the Amenity instance."""
-        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+    Inherits from SQLAlchemy Base and links to the MySQL table cities.
+
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store Cities.
+        name (sqlalchemy String): The name of the City.
+        state_id (sqlalchemy String): The state id of the City.
+    """
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", backref="cities", cascade="all, delete")
