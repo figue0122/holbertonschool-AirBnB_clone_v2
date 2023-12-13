cf226e2c690bfb44db8c427a6ad8e7ffc0b9b1cd
JCRoooD <juancarlos-99@live.com>
2023-12-13 14:52:48 -0400

    fixed task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """ Hello world with flask """
-
 from flask import Flask
 """ Flask class """
 app = Flask(__name__)
 """ Flask object """
-@app.route('/', strict_slashes=False)
+app.url_map.strict_slashes = False
 
+""" Route decorator """
+
+app.route('/')
 def hello_hbnb():
-    """ Hello world with flask """
     return 'Hello HBNB!'
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
