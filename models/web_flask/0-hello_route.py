2496b1dce53e645b43be924c350aa178094502fe
JCRoooD <juancarlos-99@live.com>
2023-12-13 14:41:34 -0400

    first task and readme

--- models/web_flask/0-hello_route.py
+++ models/web_flask/0-hello_route.py
@@ -2,9 +2,14 @@
 """ Hello world with flask """
 
 from flask import Flask
-
+""" Flask class """
 app = Flask(__name__)
+""" Flask object """
 @app.route('/', strict_slashes=False)
+
 def hello_hbnb():
     """ Hello world with flask """
     return 'Hello HBNB!'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
