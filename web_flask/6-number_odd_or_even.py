751052fc1c057e943fec9884926724c11ce600f9
JCRoooD <juancarlos-99@live.com>
2023-12-14 15:42:53 -0400

    task 6 odd and even plus template

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+""" Hello world with flask """
+
+from flask import Flask
+from flask import render_template
+""" Importing flask and render_template """
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """ Hello world with flask """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """ Hello world with flask """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c(text):
+    """ Hello world with flask """
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python/', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def python(text='is cool'):
+    """ Hello world with flask """
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    """ Hello world with flask """
+    return f"{n} is a number"
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    """ Hello world with flask """
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def number_odd_or_even(n):
+    """ Hello world with flask """
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
