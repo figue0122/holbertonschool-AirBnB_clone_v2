0429d9c94d099da9b921311a534b484c91e19a70
JCRoooD <juancarlos-99@live.com>
2023-12-14 01:58:16 -0400

    task 3  Python is cool!

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Hello world with flask """
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """ Hello world with flask """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """ Hello world with flask """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c(text):
+    """ Hello world with flask """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/')
+def python():
+    """ Hello world with flask """
+    return 'Python is cool'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
